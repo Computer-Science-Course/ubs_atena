380073d32c7ce45cb0b0560ce6ce89075a46a3b1
Mateus Santos <a.mateussm1.5@gmail.com>
2024-03-05 18:44:54 -0300

    Create menun and main layout for pages.
    Changes:
     * Create menu to be shared on all service pages.
     * Create structure for all pages.

---
+++ src/Views/Service/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/global.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/layouts.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <title>Atendimeto</title>
+</head>
+
+<body>
+    <!-- Menu -->
+    <?php
+    $is_service_active = 'active';
+    $is_pacient_active = '';
+    $is_user_active = '';
+    include_once('../../../templates/menu/index.php');
+    ?>
+    <main id="container">
+        <aside>
+            <h1>Atedimento</h1>
+        </aside>
+        <aside>
+            <label for="pacient-name">Paciente</label>
+            <input type="text" id="pacient-name">
+        </aside>
+    </main>
+</body>
+
+</html>
---
+++ templates/menu/index.php
@@ -0,0 +1,44 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="../../../public/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/buttons.css">
+</head>
+
+<menu id="main-menu">
+    <nav>
+        <!-- Service -->
+        <a href="../Service" class="box-button-icon <?php echo $is_service_active ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-plus">
+                <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
+                <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+                <path d="M9 14h6" />
+                <path d="M12 17v-6" />
+            </svg>
+        </a>
+
+        <!-- Client -->
+        <a href="../Pacient" class="box-button-icon <?php echo $is_pacient_active ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user">
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+            </svg>
+        </a>
+
+        <!-- User -->
+        <a href="../User" class="box-button-icon <?php echo $is_user_active ?>">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-laptop">
+                <path d="M20 16V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v9m16 0H4m16 0 1.28 2.55a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45L4 16" />
+            </svg>
+        </a>
+    </nav>
+    <nav>
+        <a href="" class="red-box-button-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-door-open">
+                <path d="M13 4h3a2 2 0 0 1 2 2v14" />
+                <path d="M2 20h3" />
+                <path d="M13 20h9" />
+                <path d="M10 12v.01" />
+                <path d="M13 4.562v16.157a1 1 0 0 1-1.242.97L5 20V5.562a2 2 0 0 1 1.515-1.94l4-1A2 2 0 0 1 13 4.561Z" />
+            </svg>
+        </a>
+    </nav>
+</menu>
